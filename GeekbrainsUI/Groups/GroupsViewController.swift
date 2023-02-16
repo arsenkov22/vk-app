4e127567d30971cb2b8216ab0cc9d276d915dfa5
Аleksandr Arsenkov <arsenkov22@mail.ru>
2023-02-16 12:37:55 +0300

    new

--- GeekbrainsUI/Groups/GroupsViewController.swift
+++ GeekbrainsUI/Groups/GroupsViewController.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 class GroupsViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     let cellID = "GroupTableViewCell"
     var modelGroup = ModelGroup()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         tableView.delegate = self
         tableView.dataSource = self
     }
@@ -24,21 +24,41 @@ class GroupsViewController: UIViewController {
 extension GroupsViewController: UITableViewDelegate, UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     
         return modelGroup.groups.count
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return "Мои группы"
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID) as! GroupTableViewCell
-        
+       
         let group = modelGroup.groups[indexPath.row]
         
         cell.nameGroup.text = group.name
         cell.groupImageView.image = group.image
         return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let group = modelGroup.groups[indexPath.row]
+        
+        let alert = UIAlertController(title: "\(group.name)", message: "", preferredStyle: .actionSheet)
+        let profile = UIAlertAction(title: " Открыть группу \(group.name)", style: .default){ (alert) in
+            
+        }
+        let delite = UIAlertAction(title: "Удалить группу \(group.name)", style: .default){ (alert) in
+           
+            tableView.reloadData()
+        }
+        
+        alert.addAction(UIAlertAction(title: "Отмена", style: .cancel, handler: nil))
+        
+        alert.addAction(profile)
+        alert.addAction(delite)
+        
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
--- GeekbrainsUI/NewGroups/NewGroupsViewController.swift
+++ GeekbrainsUI/NewGroups/NewGroupsViewController.swift
@@ -41,5 +41,15 @@ extension NewGroupsViewController: UITableViewDelegate, UITableViewDataSource{
         cell.newGroupImageView.image = newGroup.image
         return cell
     }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let alert = UIAlertController(title: "Хотите добавить эту группу в список своих? ", message: "", preferredStyle: .actionSheet)
+        let addAction = UIAlertAction(title: "Добавить в мои группы", style: .default) { (alert) in
+            
+        }
+
+        alert.addAction(addAction)
+        alert.addAction(UIAlertAction(title: "Отмена", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
     
 }
